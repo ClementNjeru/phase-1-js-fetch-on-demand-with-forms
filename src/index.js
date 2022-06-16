476aebf279728b809b765b21f5b47035792cd536
ClementNjeru <njeruclement36@gmail.com>
2022-06-16 18:45:16 +0300

    commit tree b00c3c7ba6b39f3ee519bc1372c170aa3ad2e09c

--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
-const init = () => {
+const submitData = {
+    userName: "Clement",
+    userEmail: "njeruclement36@gmail.com",
+  };
   
-}
+  const userCredentials = {
+    method : 'POST',
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify(submitData),
+  };
 
-document.addEventListener('DOMContentLoaded', init);
+  fetch('http://localhost:3000/users',userCredentials )
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (submitData) {
+    console.log(submitData);
+  })
