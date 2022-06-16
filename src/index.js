50d7c66a44cdc7345e6706f9b304021bfaf84ad2
ClementNjeru <njeruclement36@gmail.com>
2022-06-16 19:15:03 +0300

    updates the index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
-const init = () => {
+const submitData = () => {
+  const form = document.querySelector('form')
   
-}
+  const inputData = document.getElementById('searchByID')
+  form.addEventListener ('submit', (e) => {
+    e.preventDefault();
 
-document.addEventListener('DOMContentLoaded', init);
+    fetch(`http://localhost:3000/movies/${inputData.value}` )
+    .then (response => response.json())
+    .then (results => {const Title = document.querySelector ('h4') 
+    const Summary = document.querySelector ('p')
+  Title.innerText = results.title
+Summary.innerText = results.summary})
+  })
+} 
+
+document.addEventListener('DOMContentLoaded',submitData)
